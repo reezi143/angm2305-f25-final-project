f85108dd0061466b4aa8bbe789aa7e553058117f
Reyah Surani <reyah.surani@hotmail.com>
2025-12-08 14:33:27 -0600

    Created function "load()" to load the specified directory into the music player.

--- src/project.py
+++ src/project.py
@@ -3,3 +3,15 @@ from tkinter import filedialog
 from tkinter import *
 import pygame
 import os
+
+pygame.mixer.init()
+
+# Load Music
+def load(listbox):
+    os.chdir(filedialog.askdirectory(title='choose playlist~♡⊹♬₊⋆'))
+    listbox.delete(0, END)
+
+    tracks = os.listdir()
+
+    for track in tracks:
+        listbox.insert(END, track)
