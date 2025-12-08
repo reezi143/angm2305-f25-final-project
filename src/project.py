1efc98b3c89102a65f1c5b373f73fe141fe49942
Reyah Surani <reyah.surani@hotmail.com>
2025-12-08 15:27:35 -0600

    Created and placed skip & rewind buttons.

--- src/project.py
+++ src/project.py
@@ -98,10 +98,20 @@ pause_btn = Button(control_panel, image=pause_btn_img, bg='rosybrown1', width=30
                     command=lambda: pause_song())
 pause_btn.place(x=170, y=78)
 
+next_btn_img = PhotoImage(file='skip.png')
+next_btn = Button(control_panel, image=next_btn_img, bg='rosybrown1', width=30, height=30,
+                    command=lambda: next_song()) # doesn't work/placeholder
+next_btn.place(x=220, y=40)
+
+prev_btn_img = PhotoImage(file='prev.png')
+prev_btn = Button(control_panel, image=prev_btn_img, bg='rosybrown1', width=30, height=30,
+                    command=lambda: previous_song()) # doesn't work/placeholder
+prev_btn.place(x=120, y=40)
 
 load_btn = Button(control_panel, text='🖤♪~load playlist~♪🖤', bg='rosybrown1', fg='gray12', font=("Georgia", 13), width=35,
                   command=lambda: load(song_listbox))
 load_btn.place(x=6, y=138)
+
 
 
 app.mainloop() # keeps app open
